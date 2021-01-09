bbccbad25c292e0ece8652caf52f6a0a6f903717
andry81 <andry@inbox.ru>
2021-01-09 04:25:43 +0000

    2021.01.09: * changed: _install.bat: cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklebar/tacklebar--external_tools/trunk@19 492e1b80-0dd0-403d-a92b-fbad7d3b8131

--- _install.bat
+++ _install.bat
@@ -329,13 +329,3 @@ exit /b
 echo.^>%*
 (%*)
 exit /b
-
-:CANONICAL_PATH
-setlocal DISABLEDELAYEDEXPANSION
-for /F "eol= tokens=* delims=" %%i in ("%~2\.") do set "RETURN_VALUE=%%~fi"
-set "RETURN_VALUE=%RETURN_VALUE:\=/%"
-(
-  endlocal
-  set "%~1=%RETURN_VALUE%"
-)
-exit /b 0
